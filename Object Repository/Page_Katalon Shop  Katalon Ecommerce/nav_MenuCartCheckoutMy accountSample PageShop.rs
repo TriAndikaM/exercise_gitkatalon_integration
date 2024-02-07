2439548bb8f64d07a8f102de6d0f039cf3ab54cf
TriAndikaM <triandika3@gmail.com>
2024-02-07 10:50:26 +0700

    Penambahan test case search product

---
+++ Object Repository/Page_Katalon Shop  Katalon Ecommerce/nav_MenuCartCheckoutMy accountSample PageShop.rs
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>nav_MenuCartCheckoutMy accountSample PageShop</name>
+   <tag></tag>
+   <elementGuidId>a8fcd5b8-2509-48b2-ab35-06436842b288</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#site-navigation</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//nav[@id='site-navigation']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>nav</value>
+      <webElementGuid>52a2fedd-84ac-47cb-805a-a217b83f14bc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>site-navigation</value>
+      <webElementGuid>28475d64-3d60-480e-aad1-5783cfbf4ce2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>main-navigation</value>
+      <webElementGuid>4f0c6a4a-e086-4e4c-b04a-b21868c65462</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            			Menu
+			
+Cart
+Checkout
+My account
+Sample Page
+Shop
+
+		</value>
+      <webElementGuid>c0590839-9ece-46b8-859c-719f039ca784</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;site-navigation&quot;)</value>
+      <webElementGuid>f5522a91-0aad-463a-af6b-af9848a88663</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//nav[@id='site-navigation']</value>
+      <webElementGuid>b773ce2b-2731-44e3-951a-0599963ff751</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//header[@id='masthead']/nav</value>
+      <webElementGuid>19c62eef-405e-4ee1-82e4-cf6c7ae2ed94</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Katalon Shop'])[1]/following::nav[1]</value>
+      <webElementGuid>0388a9fc-04e0-4d9f-9e23-7dd2c437d43c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skip to content'])[1]/following::nav[1]</value>
+      <webElementGuid>cdb8410a-0e84-4bc4-996e-c9e12fe71db2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//nav</value>
+      <webElementGuid>cc15d45a-5ffd-4ca6-ac8c-7e76e2325c0c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//nav[@id = 'site-navigation' and (text() = '
+            			Menu
+			
+Cart
+Checkout
+My account
+Sample Page
+Shop
+
+		' or . = '
+            			Menu
+			
+Cart
+Checkout
+My account
+Sample Page
+Shop
+
+		')]</value>
+      <webElementGuid>5a502a5b-1e20-48a9-8747-e9aeacbb7b75</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Search Results for Flying Ninja  Katalon Shop/a_CDATA(function()for(var gfunctiontypeof O_f8795c.rs
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_CDATA(function()for(var gfunctiontypeof O_f8795c</name>
+   <tag></tag>
+   <elementGuidId>3e0e5e0d-95fa-462d-9063-7d7c779fc076</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.post-thumbnail</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//article[@id='post-54']/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>888942d7-cf01-4ded-a626-bd2fb4805bb2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>post-thumbnail</value>
+      <webElementGuid>134f2a4f-40e1-4c3f-80e8-8f32fa51d98d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://cms.demo.katalon.com/product/flying-ninja/</value>
+      <webElementGuid>f8d4caf7-3f71-4967-af73-8462909db73e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-hidden</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>1f3d3f1b-2623-4919-8eea-7ceccb46b750</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+				//&lt;![CDATA[
+(function(){for(var g=&quot;function&quot;==typeof Object.defineProperties?Object.defineProperty:function(b,c,a){if(a.get||a.set)throw new TypeError(&quot;ES3 does not support getters and setters.&quot;);b!=Array.prototype&amp;&amp;b!=Object.prototype&amp;&amp;(b[c]=a.value)},h=&quot;undefined&quot;!=typeof window&amp;&amp;window===this?this:&quot;undefined&quot;!=typeof global&amp;&amp;null!=global?global:this,k=[&quot;String&quot;,&quot;prototype&quot;,&quot;repeat&quot;],l=0;l&lt;k.length-1;l++){var m=k[l];m in h||(h[m]={});h=h[m]}var n=k[k.length-1],p=h[n],q=p?p:function(b){var c;if(null==this)throw new TypeError(&quot;The 'this' value for String.prototype.repeat must not be null or undefined&quot;);c=this+&quot;&quot;;if(0>b||1342177279&lt;b)throw new RangeError(&quot;Invalid count value&quot;);b|=0;for(var a=&quot;&quot;;b;)if(b&amp;1&amp;&amp;(a+=c),b>>>=1)c+=c;return a};q!=p&amp;&amp;null!=q&amp;&amp;g(h,n,{configurable:!0,writable:!0,value:q});var t=this;function u(b,c){var a=b.split(&quot;.&quot;),d=t;a[0]in d||!d.execScript||d.execScript(&quot;var &quot;+a[0]);for(var e;a.length&amp;&amp;(e=a.shift());)a.length||void 0===c?d[e]?d=d[e]:d=d[e]={}:d[e]=c};function v(b){var c=b.length;if(0&lt;c){for(var a=Array(c),d=0;d&lt;c;d++)a[d]=b[d];return a}return[]};function w(b){var c=window;if(c.addEventListener)c.addEventListener(&quot;load&quot;,b,!1);else if(c.attachEvent)c.attachEvent(&quot;onload&quot;,b);else{var a=c.onload;c.onload=function(){b.call(this);a&amp;&amp;a.call(this)}}};var x;function y(b,c,a,d,e){this.h=b;this.j=c;this.l=a;this.f=e;this.g={height:window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,width:window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth};this.i=d;this.b={};this.a=[];this.c={}}function z(b,c){var a,d,e=c.getAttribute(&quot;data-pagespeed-url-hash&quot;);if(a=e&amp;&amp;!(e in b.c))if(0>=c.offsetWidth&amp;&amp;0>=c.offsetHeight)a=!1;else{d=c.getBoundingClientRect();var f=document.body;a=d.top+(&quot;pageYOffset&quot;in window?window.pageYOffset:(document.documentElement||f.parentNode||f).scrollTop);d=d.left+(&quot;pageXOffset&quot;in window?window.pageXOffset:(document.documentElement||f.parentNode||f).scrollLeft);f=a.toString()+&quot;,&quot;+d;b.b.hasOwnProperty(f)?a=!1:(b.b[f]=!0,a=a&lt;=b.g.height&amp;&amp;d&lt;=b.g.width)}a&amp;&amp;(b.a.push(e),b.c[e]=!0)}y.prototype.checkImageForCriticality=function(b){b.getBoundingClientRect&amp;&amp;z(this,b)};u(&quot;pagespeed.CriticalImages.checkImageForCriticality&quot;,function(b){x.checkImageForCriticality(b)});u(&quot;pagespeed.CriticalImages.checkCriticalImages&quot;,function(){A(x)});function A(b){b.b={};for(var c=[&quot;IMG&quot;,&quot;INPUT&quot;],a=[],d=0;d&lt;c.length;++d)a=a.concat(v(document.getElementsByTagName(c[d])));if(a.length&amp;&amp;a[0].getBoundingClientRect){for(d=0;c=a[d];++d)z(b,c);a=&quot;oh=&quot;+b.l;b.f&amp;&amp;(a+=&quot;&amp;n=&quot;+b.f);if(c=!!b.a.length)for(a+=&quot;&amp;ci=&quot;+encodeURIComponent(b.a[0]),d=1;d&lt;b.a.length;++d){var e=&quot;,&quot;+encodeURIComponent(b.a[d]);131072>=a.length+e.length&amp;&amp;(a+=e)}b.i&amp;&amp;(e=&quot;&amp;rd=&quot;+encodeURIComponent(JSON.stringify(B())),131072>=a.length+e.length&amp;&amp;(a+=e),c=!0);C=a;if(c){d=b.h;b=b.j;var f;if(window.XMLHttpRequest)f=new XMLHttpRequest;else if(window.ActiveXObject)try{f=new ActiveXObject(&quot;Msxml2.XMLHTTP&quot;)}catch(r){try{f=new ActiveXObject(&quot;Microsoft.XMLHTTP&quot;)}catch(D){}}f&amp;&amp;(f.open(&quot;POST&quot;,d+(-1==d.indexOf(&quot;?&quot;)?&quot;?&quot;:&quot;&amp;&quot;)+&quot;url=&quot;+encodeURIComponent(b)),f.setRequestHeader(&quot;Content-Type&quot;,&quot;application/x-www-form-urlencoded&quot;),f.send(a))}}}function B(){var b={},c;c=document.getElementsByTagName(&quot;IMG&quot;);if(!c.length)return{};var a=c[0];if(!(&quot;naturalWidth&quot;in a&amp;&amp;&quot;naturalHeight&quot;in a))return{};for(var d=0;a=c[d];++d){var e=a.getAttribute(&quot;data-pagespeed-url-hash&quot;);e&amp;&amp;(!(e in b)&amp;&amp;0&lt;a.width&amp;&amp;0&lt;a.height&amp;&amp;0&lt;a.naturalWidth&amp;&amp;0&lt;a.naturalHeight||e in b&amp;&amp;a.width>=b[e].o&amp;&amp;a.height>=b[e].m)&amp;&amp;(b[e]={rw:a.width,rh:a.height,ow:a.naturalWidth,oh:a.naturalHeight})}return b}var C=&quot;&quot;;u(&quot;pagespeed.CriticalImages.getBeaconData&quot;,function(){return C});u(&quot;pagespeed.CriticalImages.Run&quot;,function(b,c,a,d,e,f){var r=new y(b,c,a,e,f);x=r;d&amp;&amp;w(function(){window.setTimeout(function(){A(r)},0)})});})();pagespeed.CriticalImages.Run('/mod_pagespeed_beacon','https://cms.demo.katalon.com/?s=Flying+Ninja','8Xxa2XQLv9',true,false,'dRzMaCgL_7k');
+//]]&gt;            </value>
+      <webElementGuid>fcab17c8-af12-4508-9dd8-af1625d84a7e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post-54&quot;)/a[@class=&quot;post-thumbnail&quot;]</value>
+      <webElementGuid>0d7728fa-7ae3-4b9a-a9f1-41b648c4dd98</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//article[@id='post-54']/a</value>
+      <webElementGuid>14abf74a-9005-42f0-9af9-509cbbf0396b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Flying Ninja'])[2]/following::a[1]</value>
+      <webElementGuid>3f545625-721e-46de-9e6f-48c0117721dd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Flying Ninja'])[1]/following::a[2]</value>
+      <webElementGuid>66a72536-be1b-4c04-b78f-160baf4aa390</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='on Flying Ninja'])[1]/preceding::a[1]</value>
+      <webElementGuid>f266ee1d-bbae-45ca-8440-5a9cfd3cf89a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, 'https://cms.demo.katalon.com/product/flying-ninja/')])[2]</value>
+      <webElementGuid>7c2958fb-b1cf-4b96-a282-53af644e4504</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//article/a</value>
+      <webElementGuid>ab3323a3-f9aa-4693-a664-5e83be2f4a57</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://cms.demo.katalon.com/product/flying-ninja/' and (text() = concat(&quot;
+				//&lt;![CDATA[
+(function(){for(var g=&quot;function&quot;==typeof Object.defineProperties?Object.defineProperty:function(b,c,a){if(a.get||a.set)throw new TypeError(&quot;ES3 does not support getters and setters.&quot;);b!=Array.prototype&amp;&amp;b!=Object.prototype&amp;&amp;(b[c]=a.value)},h=&quot;undefined&quot;!=typeof window&amp;&amp;window===this?this:&quot;undefined&quot;!=typeof global&amp;&amp;null!=global?global:this,k=[&quot;String&quot;,&quot;prototype&quot;,&quot;repeat&quot;],l=0;l&lt;k.length-1;l++){var m=k[l];m in h||(h[m]={});h=h[m]}var n=k[k.length-1],p=h[n],q=p?p:function(b){var c;if(null==this)throw new TypeError(&quot;The &quot; , &quot;'&quot; , &quot;this&quot; , &quot;'&quot; , &quot; value for String.prototype.repeat must not be null or undefined&quot;);c=this+&quot;&quot;;if(0>b||1342177279&lt;b)throw new RangeError(&quot;Invalid count value&quot;);b|=0;for(var a=&quot;&quot;;b;)if(b&amp;1&amp;&amp;(a+=c),b>>>=1)c+=c;return a};q!=p&amp;&amp;null!=q&amp;&amp;g(h,n,{configurable:!0,writable:!0,value:q});var t=this;function u(b,c){var a=b.split(&quot;.&quot;),d=t;a[0]in d||!d.execScript||d.execScript(&quot;var &quot;+a[0]);for(var e;a.length&amp;&amp;(e=a.shift());)a.length||void 0===c?d[e]?d=d[e]:d=d[e]={}:d[e]=c};function v(b){var c=b.length;if(0&lt;c){for(var a=Array(c),d=0;d&lt;c;d++)a[d]=b[d];return a}return[]};function w(b){var c=window;if(c.addEventListener)c.addEventListener(&quot;load&quot;,b,!1);else if(c.attachEvent)c.attachEvent(&quot;onload&quot;,b);else{var a=c.onload;c.onload=function(){b.call(this);a&amp;&amp;a.call(this)}}};var x;function y(b,c,a,d,e){this.h=b;this.j=c;this.l=a;this.f=e;this.g={height:window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,width:window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth};this.i=d;this.b={};this.a=[];this.c={}}function z(b,c){var a,d,e=c.getAttribute(&quot;data-pagespeed-url-hash&quot;);if(a=e&amp;&amp;!(e in b.c))if(0>=c.offsetWidth&amp;&amp;0>=c.offsetHeight)a=!1;else{d=c.getBoundingClientRect();var f=document.body;a=d.top+(&quot;pageYOffset&quot;in window?window.pageYOffset:(document.documentElement||f.parentNode||f).scrollTop);d=d.left+(&quot;pageXOffset&quot;in window?window.pageXOffset:(document.documentElement||f.parentNode||f).scrollLeft);f=a.toString()+&quot;,&quot;+d;b.b.hasOwnProperty(f)?a=!1:(b.b[f]=!0,a=a&lt;=b.g.height&amp;&amp;d&lt;=b.g.width)}a&amp;&amp;(b.a.push(e),b.c[e]=!0)}y.prototype.checkImageForCriticality=function(b){b.getBoundingClientRect&amp;&amp;z(this,b)};u(&quot;pagespeed.CriticalImages.checkImageForCriticality&quot;,function(b){x.checkImageForCriticality(b)});u(&quot;pagespeed.CriticalImages.checkCriticalImages&quot;,function(){A(x)});function A(b){b.b={};for(var c=[&quot;IMG&quot;,&quot;INPUT&quot;],a=[],d=0;d&lt;c.length;++d)a=a.concat(v(document.getElementsByTagName(c[d])));if(a.length&amp;&amp;a[0].getBoundingClientRect){for(d=0;c=a[d];++d)z(b,c);a=&quot;oh=&quot;+b.l;b.f&amp;&amp;(a+=&quot;&amp;n=&quot;+b.f);if(c=!!b.a.length)for(a+=&quot;&amp;ci=&quot;+encodeURIComponent(b.a[0]),d=1;d&lt;b.a.length;++d){var e=&quot;,&quot;+encodeURIComponent(b.a[d]);131072>=a.length+e.length&amp;&amp;(a+=e)}b.i&amp;&amp;(e=&quot;&amp;rd=&quot;+encodeURIComponent(JSON.stringify(B())),131072>=a.length+e.length&amp;&amp;(a+=e),c=!0);C=a;if(c){d=b.h;b=b.j;var f;if(window.XMLHttpRequest)f=new XMLHttpRequest;else if(window.ActiveXObject)try{f=new ActiveXObject(&quot;Msxml2.XMLHTTP&quot;)}catch(r){try{f=new ActiveXObject(&quot;Microsoft.XMLHTTP&quot;)}catch(D){}}f&amp;&amp;(f.open(&quot;POST&quot;,d+(-1==d.indexOf(&quot;?&quot;)?&quot;?&quot;:&quot;&amp;&quot;)+&quot;url=&quot;+encodeURIComponent(b)),f.setRequestHeader(&quot;Content-Type&quot;,&quot;application/x-www-form-urlencoded&quot;),f.send(a))}}}function B(){var b={},c;c=document.getElementsByTagName(&quot;IMG&quot;);if(!c.length)return{};var a=c[0];if(!(&quot;naturalWidth&quot;in a&amp;&amp;&quot;naturalHeight&quot;in a))return{};for(var d=0;a=c[d];++d){var e=a.getAttribute(&quot;data-pagespeed-url-hash&quot;);e&amp;&amp;(!(e in b)&amp;&amp;0&lt;a.width&amp;&amp;0&lt;a.height&amp;&amp;0&lt;a.naturalWidth&amp;&amp;0&lt;a.naturalHeight||e in b&amp;&amp;a.width>=b[e].o&amp;&amp;a.height>=b[e].m)&amp;&amp;(b[e]={rw:a.width,rh:a.height,ow:a.naturalWidth,oh:a.naturalHeight})}return b}var C=&quot;&quot;;u(&quot;pagespeed.CriticalImages.getBeaconData&quot;,function(){return C});u(&quot;pagespeed.CriticalImages.Run&quot;,function(b,c,a,d,e,f){var r=new y(b,c,a,e,f);x=r;d&amp;&amp;w(function(){window.setTimeout(function(){A(r)},0)})});})();pagespeed.CriticalImages.Run(&quot; , &quot;'&quot; , &quot;/mod_pagespeed_beacon&quot; , &quot;'&quot; , &quot;,&quot; , &quot;'&quot; , &quot;https://cms.demo.katalon.com/?s=Flying+Ninja&quot; , &quot;'&quot; , &quot;,&quot; , &quot;'&quot; , &quot;8Xxa2XQLv9&quot; , &quot;'&quot; , &quot;,true,false,&quot; , &quot;'&quot; , &quot;dRzMaCgL_7k&quot; , &quot;'&quot; , &quot;);
+//]]&gt;            &quot;) or . = concat(&quot;
+				//&lt;![CDATA[
+(function(){for(var g=&quot;function&quot;==typeof Object.defineProperties?Object.defineProperty:function(b,c,a){if(a.get||a.set)throw new TypeError(&quot;ES3 does not support getters and setters.&quot;);b!=Array.prototype&amp;&amp;b!=Object.prototype&amp;&amp;(b[c]=a.value)},h=&quot;undefined&quot;!=typeof window&amp;&amp;window===this?this:&quot;undefined&quot;!=typeof global&amp;&amp;null!=global?global:this,k=[&quot;String&quot;,&quot;prototype&quot;,&quot;repeat&quot;],l=0;l&lt;k.length-1;l++){var m=k[l];m in h||(h[m]={});h=h[m]}var n=k[k.length-1],p=h[n],q=p?p:function(b){var c;if(null==this)throw new TypeError(&quot;The &quot; , &quot;'&quot; , &quot;this&quot; , &quot;'&quot; , &quot; value for String.prototype.repeat must not be null or undefined&quot;);c=this+&quot;&quot;;if(0>b||1342177279&lt;b)throw new RangeError(&quot;Invalid count value&quot;);b|=0;for(var a=&quot;&quot;;b;)if(b&amp;1&amp;&amp;(a+=c),b>>>=1)c+=c;return a};q!=p&amp;&amp;null!=q&amp;&amp;g(h,n,{configurable:!0,writable:!0,value:q});var t=this;function u(b,c){var a=b.split(&quot;.&quot;),d=t;a[0]in d||!d.execScript||d.execScript(&quot;var &quot;+a[0]);for(var e;a.length&amp;&amp;(e=a.shift());)a.length||void 0===c?d[e]?d=d[e]:d=d[e]={}:d[e]=c};function v(b){var c=b.length;if(0&lt;c){for(var a=Array(c),d=0;d&lt;c;d++)a[d]=b[d];return a}return[]};function w(b){var c=window;if(c.addEventListener)c.addEventListener(&quot;load&quot;,b,!1);else if(c.attachEvent)c.attachEvent(&quot;onload&quot;,b);else{var a=c.onload;c.onload=function(){b.call(this);a&amp;&amp;a.call(this)}}};var x;function y(b,c,a,d,e){this.h=b;this.j=c;this.l=a;this.f=e;this.g={height:window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,width:window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth};this.i=d;this.b={};this.a=[];this.c={}}function z(b,c){var a,d,e=c.getAttribute(&quot;data-pagespeed-url-hash&quot;);if(a=e&amp;&amp;!(e in b.c))if(0>=c.offsetWidth&amp;&amp;0>=c.offsetHeight)a=!1;else{d=c.getBoundingClientRect();var f=document.body;a=d.top+(&quot;pageYOffset&quot;in window?window.pageYOffset:(document.documentElement||f.parentNode||f).scrollTop);d=d.left+(&quot;pageXOffset&quot;in window?window.pageXOffset:(document.documentElement||f.parentNode||f).scrollLeft);f=a.toString()+&quot;,&quot;+d;b.b.hasOwnProperty(f)?a=!1:(b.b[f]=!0,a=a&lt;=b.g.height&amp;&amp;d&lt;=b.g.width)}a&amp;&amp;(b.a.push(e),b.c[e]=!0)}y.prototype.checkImageForCriticality=function(b){b.getBoundingClientRect&amp;&amp;z(this,b)};u(&quot;pagespeed.CriticalImages.checkImageForCriticality&quot;,function(b){x.checkImageForCriticality(b)});u(&quot;pagespeed.CriticalImages.checkCriticalImages&quot;,function(){A(x)});function A(b){b.b={};for(var c=[&quot;IMG&quot;,&quot;INPUT&quot;],a=[],d=0;d&lt;c.length;++d)a=a.concat(v(document.getElementsByTagName(c[d])));if(a.length&amp;&amp;a[0].getBoundingClientRect){for(d=0;c=a[d];++d)z(b,c);a=&quot;oh=&quot;+b.l;b.f&amp;&amp;(a+=&quot;&amp;n=&quot;+b.f);if(c=!!b.a.length)for(a+=&quot;&amp;ci=&quot;+encodeURIComponent(b.a[0]),d=1;d&lt;b.a.length;++d){var e=&quot;,&quot;+encodeURIComponent(b.a[d]);131072>=a.length+e.length&amp;&amp;(a+=e)}b.i&amp;&amp;(e=&quot;&amp;rd=&quot;+encodeURIComponent(JSON.stringify(B())),131072>=a.length+e.length&amp;&amp;(a+=e),c=!0);C=a;if(c){d=b.h;b=b.j;var f;if(window.XMLHttpRequest)f=new XMLHttpRequest;else if(window.ActiveXObject)try{f=new ActiveXObject(&quot;Msxml2.XMLHTTP&quot;)}catch(r){try{f=new ActiveXObject(&quot;Microsoft.XMLHTTP&quot;)}catch(D){}}f&amp;&amp;(f.open(&quot;POST&quot;,d+(-1==d.indexOf(&quot;?&quot;)?&quot;?&quot;:&quot;&amp;&quot;)+&quot;url=&quot;+encodeURIComponent(b)),f.setRequestHeader(&quot;Content-Type&quot;,&quot;application/x-www-form-urlencoded&quot;),f.send(a))}}}function B(){var b={},c;c=document.getElementsByTagName(&quot;IMG&quot;);if(!c.length)return{};var a=c[0];if(!(&quot;naturalWidth&quot;in a&amp;&amp;&quot;naturalHeight&quot;in a))return{};for(var d=0;a=c[d];++d){var e=a.getAttribute(&quot;data-pagespeed-url-hash&quot;);e&amp;&amp;(!(e in b)&amp;&amp;0&lt;a.width&amp;&amp;0&lt;a.height&amp;&amp;0&lt;a.naturalWidth&amp;&amp;0&lt;a.naturalHeight||e in b&amp;&amp;a.width>=b[e].o&amp;&amp;a.height>=b[e].m)&amp;&amp;(b[e]={rw:a.width,rh:a.height,ow:a.naturalWidth,oh:a.naturalHeight})}return b}var C=&quot;&quot;;u(&quot;pagespeed.CriticalImages.getBeaconData&quot;,function(){return C});u(&quot;pagespeed.CriticalImages.Run&quot;,function(b,c,a,d,e,f){var r=new y(b,c,a,e,f);x=r;d&amp;&amp;w(function(){window.setTimeout(function(){A(r)},0)})});})();pagespeed.CriticalImages.Run(&quot; , &quot;'&quot; , &quot;/mod_pagespeed_beacon&quot; , &quot;'&quot; , &quot;,&quot; , &quot;'&quot; , &quot;https://cms.demo.katalon.com/?s=Flying+Ninja&quot; , &quot;'&quot; , &quot;,&quot; , &quot;'&quot; , &quot;8Xxa2XQLv9&quot; , &quot;'&quot; , &quot;,true,false,&quot; , &quot;'&quot; , &quot;dRzMaCgL_7k&quot; , &quot;'&quot; , &quot;);
+//]]&gt;            &quot;))]</value>
+      <webElementGuid>8278f3df-7dff-4b70-a811-c2352d5303c4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
